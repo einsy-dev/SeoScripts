26af0d6f51f44e538dc07add6383b5909eb55d86
Einsy <eins.yeager@gmail.com>
2026-04-01 12:41:35 +0400

    fix err for db call on post

--- internal/api/sheets/main.go
+++ internal/api/sheets/main.go
@@ -8,13 +8,11 @@ import (
 	"domains/internal/utils"
 	"domains/pkg/csv"
 	"encoding/json"
-	"log"
 	"maps"
 	"slices"
 	"strings"
 
 	"github.com/gofiber/fiber/v3"
-	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 )
 
@@ -81,51 +79,10 @@ func Handler(f fiber.Router) {
 
 	sheets.Use(middleware.AuthToken())
 	sheets.Post("/update", func(c fiber.Ctx) error {
-		var arrMap = csv.Arr2dToMap(c.Locals("data").([][]any))
-		var domains = getDomains(arrMap)
-
-		var dbDomains []models.Domain
-		app.DB.Where("domain in ?", domains).Preload(clause.Associations).Find(&dbDomains)
-
-		for i, v := range domains {
-			dIndex := slices.IndexFunc(dbDomains, func(el models.Domain) bool {
-				return el.Domain == v
-			})
-
-			if dIndex == -1 {
-				err := app.DB.Create(&models.Domain{Domain: v}).Error
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-				var d models.Domain
-				err = app.DB.Preload(clause.Associations).Where("domain = ?", v).First(&d).Error
-				if err != nil {
-					log.Println(err)
-					continue
-				}
-
-				dbDomains = append(dbDomains, d)
-				// shorten
-				dIndex = slices.IndexFunc(dbDomains, func(el models.Domain) bool {
-					return el.Domain == v
-				})
-			}
-
-			models.MapToDomain(arrMap[i], &dbDomains[dIndex])
-			app.DB.Session(&gorm.Session{FullSaveAssociations: true}).Save(&dbDomains)
+		if v, ok := c.Locals("data").([][]any); ok {
+			go HanlePost(v)
 		}
 
-		return c.Status(fiber.StatusOK).JSON(dbDomains)
+		return c.SendStatus(fiber.StatusAccepted)
 	})
-}
-
-func getDomains(data []map[string]any) []string {
-	var domains = make([]string, len(data))
-
-	for i, v := range data {
-		domains[i] = v["Domain"].(string)
-	}
-
-	return domains
 }
